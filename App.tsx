0dc26af0dc1bc4e86aee105176a832046c4a3d95
ramiel <ramielwan48@gmail.com>
2023-08-21 15:02:38 +0300

    refactor: simplify example

--- App.tsx
+++ App.tsx
@@ -8,14 +8,24 @@ import {
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
-  withTiming,
 } from "react-native-reanimated";
 
 export default function App() {
+  const translateX = useSharedValue(0);
+
+  const rStyle = useAnimatedStyle(() => {
+    return { transform: [{ translateX: translateX.value }] };
+  }, []);
+
+  const gesture = Gesture.Pan().onUpdate((e) => {
+    translateX.value = e.translationX;
+  });
+
   return (
     <GestureHandlerRootView style={styles.container}>
-      <Example />
-      <StatusBar style="auto" />
+      <GestureDetector gesture={gesture}>
+        <Animated.View style={[styles.ball, rStyle]} />
+      </GestureDetector>
     </GestureHandlerRootView>
   );
 }
@@ -30,42 +40,7 @@ const styles = StyleSheet.create({
   ball: {
     width: 100,
     height: 100,
-    borderRadius: 100,
     backgroundColor: "blue",
     alignSelf: "center",
   },
 });
-
-function Example() {
-  const END_POSITION = 200;
-  const onLeft = useSharedValue(true);
-  const position = useSharedValue(0);
-
-  const panGesture = Gesture.Pan()
-    .onUpdate((e) => {
-      if (onLeft.value) {
-        position.value = e.translationX;
-      } else {
-        position.value = END_POSITION + e.translationX;
-      }
-    })
-    .onEnd((e) => {
-      if (position.value > END_POSITION / 2) {
-        position.value = withTiming(END_POSITION, { duration: 100 });
-        onLeft.value = false;
-      } else {
-        position.value = withTiming(0, { duration: 100 });
-        onLeft.value = true;
-      }
-    });
-
-  const animatedStyle = useAnimatedStyle(() => ({
-    transform: [{ translateX: position.value }],
-  }));
-
-  return (
-    <GestureDetector gesture={panGesture}>
-      <Animated.View style={[styles.ball, animatedStyle]} />
-    </GestureDetector>
-  );
-}
